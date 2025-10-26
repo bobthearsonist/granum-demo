1d9022d16d7946de534c39921dbae9ad89448523
bobthearsonist <bobthearsonist@users.noreply.github.com>
2025-10-25 21:35:32 -0400

    feat: implement happy path initial tests for UserController with generic support for Customer and Contractor

--- apps/backend/Granum.Tests/Features/User/UserControllerTests.cs
+++ apps/backend/Granum.Tests/Features/User/UserControllerTests.cs
@@ -1,7 +1,48 @@
+using FluentAssertions;
+using Granum.Api.Features.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
 namespace Granum.Tests.Features.User;
 
-[TestFixture]
-public class UserControllerTests
+[TestFixture(typeof(Customer), typeof(CustomersController))]
+[TestFixture(typeof(Contractor), typeof(ContractorsController))]
+public class UserControllerBaseTests<TUser, TController>
+    where TUser : Api.Features.User.User, new()
+    where TController : UserControllerBase<TUser>
 {
-    // TODO: Add controller tests here
+    private IUserService<Api.Features.User.User> _mockUserService;
+    private ILogger _mockLogger;
+    private UserControllerBase<Api.Features.User.User> _controller;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        _mockUserService = Substitute.For<IUserService<Api.Features.User.User>>();
+        _mockLogger = Substitute.For<ILogger>();
+        _controller = Substitute.ForPartsOf<UserControllerBase<Api.Features.User.User>>(
+            _mockUserService,
+            _mockLogger
+        );
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        _controller.Dispose();
+    }
+
+    [Test]
+    public async Task GetAllUsers_ReturnsListOfUsers()
+    {
+        var users = new[] { new TUser{Id = 1, Name = "bob"}, new TUser{Id = 2, Name = "susan"} };
+        _mockUserService.GetAllAsync().Returns(users);
+
+        var result = await _controller.GetAll();
+
+        result.Should().BeOfType<OkObjectResult>();
+        var okResult = result as OkObjectResult;
+        okResult!.Value.Should().BeEquivalentTo(users);
+    }
 }
